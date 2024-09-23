bc6a0403596c43f9ce2436d8a47017d5a811fb24
K1ngJadon2012 <tdawit31@yahoo.com>
2024-09-22 23:37:42 -0700

    setting up backend payment w/ function

---
+++ amazon-clone/functions/index.js
@@ -0,0 +1,41 @@
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")( process.env.STRIPE_KEY);
+
+
+
+const app = express();
+app.use(cors({origin: "true"}));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Hello from FirebaseIt is working !",});
+
+    });
+
+app.post("/payments/create", async (req, res) => {
+    const total =  req.query.total; // req.query.total;
+    if (total > 0) {
+        // console.log("Payment Request Recieved for this amount >>> ", total);
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: total,
+            currency: "usd"
+        })
+ 
+    res.status(201).json({
+        clientSecret: paymentIntent.client_secret,
+    });
+
+    } else {
+        res.status(403).json({error: "Payment Failed"});
+
+}
+    });
+    
+exports.api = onRequest(app)
