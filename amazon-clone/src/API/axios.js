dfe6dad42eb981baf55c12ae71830c292cd627dd
K1ngJadon2012 <tdawit31@yahoo.com>
2024-09-26 16:10:43 -0700

    commented the base url of frontend

--- amazon-clone/src/API/axios.js
+++ amazon-clone/src/API/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // local instance of firebase function
-    baseURL: "http://127.0.0.1:5001/clone-872d5/us-central1/api",
+    // baseURL: "http://127.0.0.1:5001/clone-872d5/us-central1/api",
 
     // deployed version of firebase function
     //<<<<<<<<<<<Missing  URL>>>>>>> 
