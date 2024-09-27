129232fc7f73b6ee53247226ea56193e2288f135
K1ngJadon2012 <tdawit31@yahoo.com>
2024-09-27 15:35:55 -0700

    adjustment in product detail

--- amazon-clone/src/Pages/ProductDetail/ProductDetail.jsx
+++ amazon-clone/src/Pages/ProductDetail/ProductDetail.jsx
@@ -3,7 +3,7 @@ import classes from './ProductDetail.module.css'
 import LayOut from '../../Components/LayOut/LayOut'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import { productUrl } from '../../Api/endPoints'
+import { productUrl } from '../../API/endPoints'
 import ProductCard from '../../Components/Product/ProductCard'
 import Loader from '../../Components/Loader/Loader'
 
